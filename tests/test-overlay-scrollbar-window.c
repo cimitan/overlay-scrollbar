50a51f6c6b9158ddb7e30e54cac27817dd4fc6e0
Andrea Cimitan <andrea.cimitan@canonical.com>
2011-02-08 13:30:29 +0100

    Compiles tests directory

--- tests/test-overlay-scrollbar-window.c
+++ tests/test-overlay-scrollbar-window.c
@@ -27,7 +27,7 @@
 
 #include <gtk/gtk.h>
 
-#include <overlay-scrollbar.h>
+#include "overlay-scrollbar.h"
 
 static void window_destroy_cb (GtkWidget *widget,
                                gpointer   user_data);
