2019bb1362a585511478f41c3bef6061032320aa
Andrea Cimitan <andrea.cimitan@canonical.com>
2011-02-11 20:01:24 +0100

    DRAWS ON GDK_WINDOW_CHILD! that could be a good news, I love those test apps ;)

--- tests/overlay-scrollbar-test-pixmap.c
+++ tests/overlay-scrollbar-test-pixmap.c
@@ -91,12 +91,17 @@ pixmap_draw (GdkPixmap *pixmap)
 
   cr_surface = cairo_create (surface);
 
-  os_cairo_draw_rounded_rect (cr_surface, 10, 10, width - 20, height - 20, 20);
-  cairo_set_source_rgba (cr_surface, 1.0, 0.6, 0.6, 0.5);
+  cairo_set_operator (cr_surface, CAIRO_OPERATOR_SOURCE);
+  cairo_set_source_rgba (cr_surface, 1.0, 1.0, 1.0, 1.0);
+  cairo_paint (cr_surface);
+
+  cairo_set_operator (cr_surface, CAIRO_OPERATOR_OVER);
+  os_cairo_draw_rounded_rect (cr_surface, 1, 1, width - 2, height - 2, 20);
+  cairo_set_source_rgba (cr_surface, 1.0, 0.6, 0.6, 0.8);
   cairo_fill_preserve (cr_surface);
 
   cairo_set_line_width (cr_surface, 2.0);
-  cairo_set_source_rgba (cr_surface, 1.0, 0.8, 0.8, 1.0);
+  cairo_set_source_rgba (cr_surface, 0.8, 0.4, 0.4, 1.0);
   cairo_stroke (cr_surface);
 
   status = cairo_surface_write_to_png (surface, "/tmp/overlay-scrollbar-test-pixmap.png");
@@ -128,21 +133,27 @@ int
 main (int   argc,
       char *argv[])
 {
+  GtkAllocation allocation;
   GtkWidget *window;
   GtkWidget *vbox;
   GtkWidget *frame;
+  GtkWidget *frame_window;
   GtkWidget *image;
   GdkPixmap *pixmap;
+  GdkWindowAttr attributes;
+  GdkWindow *child_window;
+  gint attributes_mask;
 
   gtk_init (&argc, &argv);
 
   /* pixmap */
-  pixmap = gdk_pixmap_new (NULL, 100, 100, 24);
+  pixmap = gdk_pixmap_new (NULL, 120, 100, 24);
   pixmap_draw (pixmap);
 
   /* window */
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-/*  gtk_window_set_default_size (GTK_WINDOW (window), 200, 400);*/
+  gtk_window_set_has_resize_grip (GTK_WINDOW (window), FALSE);
+  gtk_window_set_default_size (GTK_WINDOW (window), 160, 300);
   gtk_window_set_title (GTK_WINDOW (window), "Test GdkDrawable and GdkPixmap");
 
   /* vbox */
@@ -151,11 +162,17 @@ main (int   argc,
   /* frame */
   frame = gtk_frame_new ("GdkPixmap");
 
+  /* frame_window */
+  frame_window = gtk_frame_new ("GDK_WINDOW_CHILD");
+
   /* image */
   image = gtk_image_new_from_pixmap (pixmap, NULL);
 
+  /* containers */
+  gtk_container_set_border_width (GTK_CONTAINER (window), 10);
   gtk_container_add (GTK_CONTAINER (frame), image);
   gtk_container_add (GTK_CONTAINER (vbox), frame);
+  gtk_container_add (GTK_CONTAINER (vbox), frame_window);
   gtk_container_add (GTK_CONTAINER (window), vbox);
 
   /* signals */
@@ -163,6 +180,21 @@ main (int   argc,
                     G_CALLBACK (window_destroy_cb), NULL);
 
   gtk_widget_show_all (window);
+
+  gtk_widget_get_allocation (frame_window, &allocation);
+
+  /* child_window */
+  attributes.x = allocation.x+14;
+  attributes.y = allocation.y+20;
+  attributes.width = 120;
+  attributes.height = 100;
+  attributes.wclass = GDK_INPUT_OUTPUT;
+  attributes.window_type = GDK_WINDOW_CHILD;
+  attributes_mask = GDK_WA_X | GDK_WA_Y;
+  child_window = gdk_window_new (gtk_widget_get_window (window), &attributes, attributes_mask);
+
+  gdk_window_set_back_pixmap (child_window, pixmap, FALSE);
+  gdk_window_show (child_window);
 
   gtk_main ();
 }
