8daa247df749908b22df8ee544f3299345b88bf0
Loïc Molinari <loic.molinari@canonical.com>
2011-03-01 00:03:34 +0100

    Added main and versionning headers

---
+++ os/os.h
@@ -0,0 +1,32 @@
+/* liboverlay-scrollbar
+ *
+ * Copyright © 2011 Canonical Ltd
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Authored by Andrea Cimitan <andrea.cimitan@canonical.com>
+ */
+
+#ifndef __HAVE_OS_H__
+#define __HAVE_OS_H__
+
+#define __OS_H_INSIDE__
+
+#include "os-scrollbar.h"
+
+#undef __OS_H_INSIDE__
+
+#endif /* __HAVE_OS_H__ */
