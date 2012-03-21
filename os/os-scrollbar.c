82784589fc88319d36d86d64227513d5c3f2ecd1
Andrea Cimitan <andrea.cimitan@canonical.com>
2012-03-21 13:30:03 +0000

    Fix 907111 by removing the timeout on dispose

--- os/os-scrollbar.c
+++ os/os-scrollbar.c
@@ -3180,6 +3180,12 @@ os_scrollbar_dispose (GObject *object)
       priv->source_hide_thumb_id = 0;
     }
 
+  if (priv->source_show_thumb_id != 0)
+    {
+      g_source_remove (priv->source_show_thumb_id);
+      priv->source_show_thumb_id = 0;
+    }
+
   if (priv->source_unlock_thumb_id != 0)
     {
       g_source_remove (priv->source_unlock_thumb_id);
